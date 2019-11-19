e10fe85309c9f2fcdd454bc7e570120821fda2b5
Firemark <marpiechula@gmail.com>
2019-11-19 21:34:25 +0100

    add custom markdown plugins

--- content/articles/index.md
+++ content/articles/index.md
@@ -13,33 +13,33 @@ Lang: pl
 ### SECTION ###
 # Co to jest Coding Dojo?
 
-<div class="col-2-1" markdown="1">
+### 2-1 COL ###
 ![cds](/images/photos/1.png){: .border }
-</div>
 
-<div class="col-2-2" markdown="1">
+### 2-2 COL ###
 Coding Dojo to spotkanie służące wspólnemu doskonaleniu umiejętności programistycznych. Nazwa wywodzi się od japońskiego określenia dōjō oznaczającego miejsce treningu i głębokiej medytacji, w którym spotykali się adepci sztuk walki i buddyjscy mnisi zen.
 
 W trakcie coding dojo rozwiązujemy code kata. Kata w sztukach walki to pewna technika, która jest powtarzana tysiące razy przez adeptów owych dyscyplin. Pozwala ona, tak jak ćwiczenie pojedynczych ciosów, rozwijać swoje umiejętności poprzez powtarzanie tych samych sekwencji. Code kata w myśl oryginalnej idei jest ćwiczeniem polegającym na rozwiązaniu prostego zadania programistycznego w najlepszy możliwy sposób.
 
 [czytaj <br/> więcej](?){: .btn } 
-</div>
+
+### END COL ###
 
 ### RED-PATTERN SECTION ###
 # Co zyskasz?
 
-<div class="col-2-1" markdown="1">
+### 2-1 COL ###
 * Przećwiczysz dobre techniki programowania,
 * Świadomie rozwiniesz swój warsztat programisty,
 * Poznasz programowanie w parach, <!--- xD -->
 * Zobaczysz jak można inaczej podejść do problemu,
 * Dostaniesz konstruktywny feedback,
 * Poznasz innych, zaangażowanych programistów w okolicy.
-</div>
 
-<div class="col-2-2" markdown="1">
+### 2-2 COL ###
 ![cds](/images/photos/2.png){: .border }
-</div>
+
+### END COL ###
 
 ### SECTION ###
 # WSZYSTKIE WYDARZENIA
@@ -49,14 +49,14 @@ W trakcie coding dojo rozwiązujemy code kata. Kata w sztukach walki to pewna te
 ### SECTION ###
 # Prowadź sam Coding dojo!
 
-<div class="col-2-1" markdown="1">
+### 2-1 COL ###
 ![cds](/images/photos/3.png){: .border }
-</div>
 
-<div class="col-2-2" markdown="1">
+### 2-2 COL ###
 Chcesz zorganizować dojo w innym mieście na Śląsku, w Polsce lub w swojej firmie? Dostaniesz od nas wszystkie potrzebne materiały oraz wsparcie
 [zorganizuj <br/> dojo](?){: .btn }
-</div>
+
+### END COL ###
 
 ### SECTION ###
 # PARTNERZY
--- markdown_plugins.py
+++ markdown_plugins.py
@@ -3,36 +3,41 @@ from markdown import Extension
 import re
 
 
-class SectionPreprocessor(Preprocessor):
-    END_SECTION_LINES = ['</div>', '</section>']
-    section_pattern = re.compile(r'### +([\w-]+ +)?SECTION +###', re.IGNORECASE)
+class DojoPreprocessor(Preprocessor):
+    END_LINES = []
+    pattern = re.compile(r'---', re.IGNORECASE)
 
     def run(self, lines):
         state = 'START'
         new_lines = []
         for line in lines:
-            match = self.section_pattern.match(line.strip())
+            match = self.pattern.match(line.strip())
             if not match:
                 new_lines.append(line) 
                 continue
-            section_name = (match.group(1) or '').strip()
+            section_name = match.group(1)
             state, additional_lines = self.get_next_line(state, section_name)
             new_lines += additional_lines
         if state is not None:  # close sections
-            new_lines += self.END_SECTION_LINES
+            new_lines += self.END_LINES
         return new_lines
 
+class SectionPreprocessor(DojoPreprocessor):
+    END_LINES = ['</div>', '</section>']
+    pattern = re.compile(r'### +([\w-]+ +)?SECTION +###', re.IGNORECASE)
+
     def get_next_line(self, state, section_name):
-        prefix = self.END_SECTION_LINES if state != 'START' else []
+        prefix = self.END_LINES if state != 'START' else []
 
         def section(classes=None):
             if classes is None:
                 lines = ['<section markdown="1">', '<div>']
             else:
                 lines = [f'<section class="{classes}" markdown="1">', '<div>']
-            return prefix + lines
+            return prefix + [''] + lines
 
         if section_name:
+            section_name = section_name.strip().upper()
             if section_name == 'RED':
                 return 'RED', section('red')
             if section_name in 'RED-PATTERN':
@@ -51,9 +56,22 @@ class SectionPreprocessor(Preprocessor):
         return 'END', []  # fallback
 
 
+class ColPreprocessor(DojoPreprocessor):
+    END_LINES = ['</div>']
+    pattern = re.compile(r'### +([\w-]+) +COL +###', re.IGNORECASE)
+
+    def get_next_line(self, state, section_name):
+        section_name = section_name.lower()
+        prefix = self.END_LINES if state != 'START' else []
+        if section_name == 'end':
+            return 'START', prefix
+        return 'SET', prefix + [''] + [f'<div class="col-{section_name}" markdown="1">']
+
+
 class DojoExtension(Extension):
     def extendMarkdown(self, md):
         md.preprocessors.register(SectionPreprocessor(md), 'section-preprocessor', 175)
+        md.preprocessors.register(ColPreprocessor(md), 'col-preprocessor', 170)
 
 
 makeExtension = DojoExtension # need to register extension
